ca8a701dd80426fbf25070448d3d0a3234b0750f
TasosFrago <tasosfragkopoulos03@gmail.com>
2025-04-28 21:53:00 +0300

    Sync

--- run.sh
+++ run.sh
@@ -1,17 +1,17 @@
-python3 run.py -i ../gray/mountain30000.raw -o out.raw -W 30000 -H 30000 --iters 1
-python3 run.py -i ../gray/mountain30000.raw -o out.raw -W 30000 -H 30000 --iters 4
-
-python3 run.py -i ../gray/mountain8192.raw -o out.raw -W 8192 -H 8192 --iters 1
-python3 run.py -i ../gray/mountain8192.raw -o out.raw -W 8192 -H 8192 --iters 4
-
-python3 run.py -i ../gray/mountain4096.raw -o out.raw -W 4096 -H 4096 --iters 1
-python3 run.py -i ../gray/mountain4096.raw -o out.raw -W 4096 -H 4096 --iters 4
-
-python3 run.py -i ../gray/mountain2048.raw -o out.raw -W 2048 -H 2048 --iters 1
-python3 run.py -i ../gray/mountain2048.raw -o out.raw -W 2048 -H 2048 --iters 4
+python3 run.py -i ../gray/mri512.raw -o out.raw -W 512 -H 512 --iters 1
+python3 run.py -i ../gray/mri512.raw -o out.raw -W 512 -H 512 --iters 4
 
 python3 run.py -i ../gray/mountain1024.raw -o out.raw -W 1024 -H 1024 --iters 1
 python3 run.py -i ../gray/mountain1024.raw -o out.raw -W 1024 -H 1024 --iters 4
 
-python3 run.py -i ../gray/mri512.raw -o out.raw -W 512 -H 512 --iters 1
-python3 run.py -i ../gray/mri512.raw -o out.raw -W 512 -H 512 --iters 4
+python3 run.py -i ../gray/mountain2048.raw -o out.raw -W 2048 -H 2048 --iters 1
+python3 run.py -i ../gray/mountain2048.raw -o out.raw -W 2048 -H 2048 --iters 4
+
+python3 run.py -i ../gray/mountain4096.raw -o out.raw -W 4096 -H 4096 --iters 1
+python3 run.py -i ../gray/mountain4096.raw -o out.raw -W 4096 -H 4096 --iters 4
+
+python3 run.py -i ../gray/mountain8192.raw -o out.raw -W 8192 -H 8192 --iters 1
+python3 run.py -i ../gray/mountain8192.raw -o out.raw -W 8192 -H 8192 --iters 4
+
+python3 run.py -i ../gray/mountain30000.raw -o out.raw -W 30000 -H 30000 --iters 1
+python3 run.py -i ../gray/mountain30000.raw -o out.raw -W 30000 -H 30000 --iters 4
